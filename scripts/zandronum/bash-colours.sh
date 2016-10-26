f01570013a1bfba0c258dfb89f2648a2a67dcc35
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2016-10-26 21:03:31 +0200

    Add files via upload

---
+++ scripts/zandronum/bash-colours.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+tput setaf 0;
+echo "###################"
+tput setaf 1;
+echo "###################"
+tput setaf 2;
+echo "###################"
+tput setaf 3;
+echo "###################"
+tput setaf 4;
+echo "###################"
+tput setaf 5;
+echo "###################"
+tput setaf 6;
+echo "###################"
+tput setaf 7;
+echo "###################"
+tput setaf 8;
+echo "###################"
+tput setaf 9;
+echo "###################"
+tput setaf 10;
+echo "###################"
+tput setaf 11;
+echo "###################"
+tput setaf 12;
+echo "###################"
+tput setaf 13;
+echo "###################"
+tput setaf 14;
+echo "###################"
+tput setaf 15;
+echo "###################"
+tput setaf 16;
+echo "###################"
+tput setaf 17;
+echo "###################"
+tput setaf 18;
+echo "###################"
+tput setaf 19;
+echo "###################"
+tput setaf 20;
+echo "###################"
+tput setaf 21;
+echo "###################"
+tput setaf 22;
+echo "###################"
+tput setaf 23;
+echo "###################"
+tput setaf 24;
+echo "###################"
+tput setaf 25;
+echo "###################"
+tput setaf 26;
+echo "###################"
+tput setaf 27;
+echo "###################"
+tput setaf 28;
+echo "###################"
+tput setaf 29;
+echo "###################"
+tput setaf 30;
+echo "###################"
+tput setaf 31;
+echo "###################"
+tput setaf 32;
+echo "###################"
+tput setaf 33;
+echo "###################"
+tput setaf 34;
+echo "###################"
+tput setaf 35;
+echo "###################"
+tput setaf 36;
+echo "###################"
+tput setaf 37;
+echo "###################"
+tput setaf 38;
+echo "###################"
+tput setaf 39;
+echo "###################"
+tput setaf 40;
+echo "###################"
+tput setaf 41;
+echo "###################"
+tput setaf 42;
+echo "###################"
+tput setaf 43;
+echo "###################"
+tput setaf 44;
+echo "###################"
+tput setaf 45;
+echo "###################"
+tput setaf 46;
+echo "###################"
+tput setaf 47;
+echo "###################"
+tput setaf 48;
+echo "###################"
+tput setaf 49;
+echo "###################"
+tput setaf 50;
+echo "###################"
+tput setaf 51;
+echo "###################"
+tput setaf 52;
+echo "###################"
+tput setaf 53;
+echo "###################"
+tput setaf 54;
+echo "###################"
+tput setaf 55;
+echo "###################"
+tput setaf 56;
+echo "###################"
+tput setaf 57;
+echo "###################"
+tput setaf 58;
+echo "###################"
+tput setaf 59;
+echo "###################"
+tput setaf 60;
+echo "###################"
+tput setaf 61;
+echo "###################"
+tput setaf 62;
+echo "###################"
+tput setaf 63;
+echo "###################"
+tput setaf 64;
+echo "###################"
+tput setaf 65;
+echo "###################"
+tput setaf 66;
+echo "###################"
+tput setaf 67;
+echo "###################"
+tput setaf 68;
+echo "###################"
+tput setaf 69;
+echo "###################"
+tput setaf 70;
+echo "###################"
+tput setaf 71;
+echo "###################"
+tput setaf 72;
+echo "###################"
+tput setaf 73;
+echo "###################"
+tput setaf 74;
+echo "###################"
+tput setaf 75;
+echo "###################"
+tput setaf 76;
+echo "###################"
+tput setaf 77;
+echo "###################"
+tput setaf 78;
+echo "###################"
+tput setaf 79;
+echo "###################"
+tput setaf 80;
+echo "###################"
+tput setaf 81;
+echo "###################"
+tput setaf 82;
+echo "###################"
+tput setaf 83;
+echo "###################"
+tput setaf 84;
+echo "###################"
+tput setaf 85;
+echo "###################"
+tput setaf 86;
+echo "###################"
+tput setaf 87;
+echo "###################"
+tput setaf 88;
+echo "###################"
+tput setaf 89;
+echo "###################"
+tput setaf 90;
+echo "###################"
+tput setaf 91;
+echo "###################"
+tput setaf 92;
+echo "###################"
+tput setaf 93;
+echo "###################"
+tput setaf 94;
+echo "###################"
+tput setaf 95;
+echo "###################"
+tput setaf 96;
+echo "###################"
+tput setaf 97;
+echo "###################"
+tput setaf 98;
+echo "###################"
+tput setaf 99;
+echo "###################"
+tput setaf 100;
+echo "########100########"
+
+
