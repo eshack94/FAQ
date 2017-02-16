cc9c2cd4875d28c439bc3c1eca0a11fea187304a
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2017-02-16 12:39:48 +0100

    Create mpv_conf.sh

---
+++ backups/mpv/mpv_conf.sh
@@ -0,0 +1,6 @@
+#!bin/bash
+
+mkdir -p /home/$USER/.config/mpv/
+cd /home/$USER/.config/mpv/
+wget -O input.conf https://raw.githubusercontent.com/brandleesee/blc/master/backups/mpv/input.conf
+cd /home/$USER/
