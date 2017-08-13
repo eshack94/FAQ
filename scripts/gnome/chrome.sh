ab0bdc395533d47e3f71a44f51787b8b73ad3b1b
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2017-08-13 07:47:42 +0200

    Update and rename chrome_beta.sh to chrome.sh

---
+++ scripts/gnome/chrome.sh
@@ -0,0 +1,6 @@
+#!BIN/BASH
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update 
+sudo apt-get -y install google-chrome-stable
