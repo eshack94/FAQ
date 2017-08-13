fb6795a3716da5f2b5c89bb6de0e2433504cfeae
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2017-08-13 08:24:22 +0200

    Update chrome.sh

--- scripts/gnome/chrome.sh
+++ scripts/gnome/chrome.sh
@@ -1,6 +1,8 @@
 #!BIN/BASH
 
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub
+gpg --keyserver https://dl.google.com/linux/linux_signing_key.pub --recv-keys 7FAC5991
+gpg --export --armor 7FAC5991 | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update 
 sudo apt-get -y install google-chrome-stable
