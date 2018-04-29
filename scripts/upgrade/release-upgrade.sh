7d765fa19cd7d97aa1c367f0823367df6db494c3
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2018-04-29 10:00:00 +0200

    Create release-upgrade.sh

---
+++ scripts/upgrade/release-upgrade.sh
@@ -0,0 +1,17 @@
+#!BIN/BASH
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+sudo apt clean
+sudo apt autoremove -y
+sudo apt purge -y
+
+{
+  if [[ dpkg -s update-manager-core == "install ok installed" ]]; then
+    sudo do-release-upgrade
+  else
+    sudo apt install update-manager-core -y
+    sudo do-release-upgrade
+  fi
+}
