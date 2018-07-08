80bba6ab32febf0d18dbda85e69a67d630dfe663
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2018-07-08 17:35:54 +0200

    Update strip_install.sh

--- scripts/lubuntu/strip_install.sh
+++ scripts/lubuntu/strip_install.sh
@@ -1,25 +1,18 @@
 #!/bin/bash
 
+apt -y update 
 apt -y purge *transmission* 
-apt -y purge simple-scan 
 apt -y purge *pidgin* 
 apt -y purge *guvcview* 
 apt -y purge *abiword* 
 apt -y purge *gnumeric* 
 apt -y purge *xfburn* 
-apt -y purge mtpaint 
 apt -y purge *sylpheed* 
 apt -y purge *evince* 
 apt -y purge *hardinfo* 
-apt -y purge lubuntu-software-center 
-apt -y purge xpad 
-apt -y purge usb-creator-gtk 
-apt -y purge blueman 
-
-apt -y autoremove 
-apt clean 
-
-apt -y update 
+apt -y purge *thunderbird*
+apt -y purge lubuntu-software-center usb-creator-gtk simple-scan xpad mtpaint blueman 
+apt -y install gdebi
 apt -y full-upgrade 
-apt -y autoremove 
+apt -y autoremove --purge
 apt clean 
