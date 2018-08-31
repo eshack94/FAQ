fc00285c11b0728b28d0ada2bf9f9baa1e1c7f4d
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2018-08-31 19:44:16 +0200

    Rewritten from scratch.
    https://forum.pinguyos.com/Thread-Automatic-Updating

--- scripts/maintenance/u.sh
+++ scripts/maintenance/u.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-apt -qqq update
-apt -qqq full-upgrade
-apt -qqq autoremove --purge
-apt clean
+/usr/bin/dpkg --configure -a
+rm /var/lib/apt/lists/* -vf
+/usr/bin/apt --yes --assume-yes update
+/usr/bin/apt --yes --assume-yes full-upgrade
+/usr/bin/apt install -y -f
+/usr/bin/apt --yes --assume-yes autoremove --purge
+/usr/bin/apt clean
 rm -rf ~/.local/share/Trash/*
