d14e80e3038d598249283f01f97828d003872fb0
Brandon Camilleri <brandon.camilleri.90@gmail.com>
2018-10-01 16:54:55 +0200

    Update u.sh

--- scripts/maintenance/u.sh
+++ scripts/maintenance/u.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
 /usr/bin/dpkg --configure -a
-sleep 1
-rm /var/lib/apt/lists/* -vf
-sleep 1
 /usr/bin/apt --yes --assume-yes update
-sleep 1
 /usr/bin/apt --yes --assume-yes full-upgrade
-sleep 1
 /usr/bin/apt install -y -f
-sleep 1
 /usr/bin/apt --yes --assume-yes autoremove --purge
-sleep 1
 /usr/bin/apt clean
-sleep 1
 /usr/bin/dpkg -l | grep '^rc' | awk '{print $2}' | xargs /usr/bin/apt --yes --assume-yes purge
-sleep 1
 rm -rf ~/.cache/thumbnails/*
-sleep 1
 rm -rf ~/.local/share/Trash/*
-sleep 1
-rm -rf /tmp/*
-sleep 1
